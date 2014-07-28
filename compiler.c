a20204034ee697737ec873f351ae0e9dfb4254b8
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-28 20:58:14 +0300

    fix(maman/*): remove unneccery files

---
+++ compiler.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  maman14
+//
+//  Created by Ariel Mashraki on 7/28/14.
+//  Copyright (c) 2014 Ariel Mashraki. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    // insert code here...
+    printf("bar");
+    return 0;
+}
+
