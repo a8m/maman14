e640524d2ca33b252a43c9e52b7efa69fca8717e
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-30 20:48:14 +0300

    feat(hash_table): register op actions

--- hash_table.h
+++ hash_table.h
@@ -7,9 +7,27 @@
  * =========================================================================================
  */
 
+/* >>>>>>>>>>>>>>>>>>>>>>>> DEFINE OP ACTIONS >>>>>>>>>>>>>>>>>>>>>>>> */
+#define MOV 0
+#define CMP 1
+#define ADD 2
+#define SUB 3
+#define NOT 4
+#define CLR 5
+#define LEA 6
+#define INC 7
+#define DEC 8
+#define JMP 9
+#define BNE 10
+#define RED 11
+#define PRN 12
+#define JSR 13
+#define RTS 14
+#define STOP 15
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>> END DEFINE >>>>>>>>>>>>>>>>>>>>>>>>>>> */
+
 typedef struct node {
     struct node *next; /* pointer */
     char *name; /* defined name */
     char *defn; /* decimal adress */
 } hash_node;
-
