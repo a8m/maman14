33d1b75c8ca0dd27be6c44f788d0d9c31d91497c
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-30 20:35:58 +0300

    feat(common.h): create

---
+++ common.h
@@ -0,0 +1,14 @@
+//
+//  common.h
+//  maman14
+//
+//  Created by Ariel Mashraki on 7/28/14.
+//  Copyright (c) 2014 Ariel Mashraki. All rights reserved.
+//
+
+#ifndef maman14_common_h
+#define maman14_common_h
+
+
+
+#endif
