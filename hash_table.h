4f269d11df3a70d07a393036d262585de66f4450
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-30 20:38:36 +0300

    fix(hash_table): add head comments

--- hash_table.h
+++ hash_table.h
@@ -1,4 +1,13 @@
-typedef struct node { 
+/*
+ * =========================================================================================
+ * name        : maman_14/hash_table.h
+ * author      : Ariel Mashraki, Osnat Izic
+ * email       :
+ * description :
+ * =========================================================================================
+ */
+
+typedef struct node {
     struct node *next; /* pointer */
     char *name; /* defined name */
     char *defn; /* decimal adress */
