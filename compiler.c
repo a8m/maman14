6b044c55af040fe7873107bc49aff7d688cdeeaf
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-30 20:38:09 +0300

    feat(main): compiler main funciton

--- compiler.c
+++ compiler.c
@@ -1,18 +1,44 @@
-//
-//  main.c
-//  maman14
-//
-//  Created by Ariel Mashraki on 7/28/14.
-//  Copyright (c) 2014 Ariel Mashraki. All rights reserved.
-//
+/*
+ * =========================================================================================
+ * name        : maman_14/compiler.c
+ * author      : Ariel Mashraki, Osnat Izic
+ * email       :
+ * description :
+ * =========================================================================================
+ */
 
-#include <stdio.h>
+#include "common.h"
 
-int main(int argc, const char * argv[])
+int main(int argc, char **argv)
 {
     
-    // insert code here...
-    printf("bar");
-    return 0;
+    int i = 0;
+    FILE *fp;
+    char file_name[MAX_FILE_NAME];
+    
+    if (argc == 1) {
+        printf(NO_FILES);
+        return FALSE;
+    }
+    /*loop over the file names arguments*/
+    while (++i < argc) {
+        /*create the string to open the file*/
+        sprintf(file_name, "%s.as", argv[i]);
+        fp = fopen(file_name, "r");
+        
+        /*if there is any problem with the file*/
+        if(!fp) {
+            printf(FILE_ERROR, file_name);
+            return FALSE;
+        }
+        
+        /* allocate memory for second phase fprintf*/
+        
+        printf("success %s", file_name);
+    }
+    
+    
+    
+    return EXIT;
 }
 
