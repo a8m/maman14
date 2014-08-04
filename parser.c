f40403651c183274decafb8259af36b413c51cd5
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-04 20:06:07 +0300

    feat(parser.h/c): add files

---
+++ parser.c
@@ -0,0 +1,9 @@
+//
+//  parser.c
+//  maman14
+//
+//  Created by Ariel Mashraki on 8/4/14.
+//  Copyright (c) 2014 Ariel Mashraki. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ parser.h
@@ -0,0 +1,14 @@
+//
+//  parser.h
+//  maman14
+//
+//  Created by Ariel Mashraki on 8/4/14.
+//  Copyright (c) 2014 Ariel Mashraki. All rights reserved.
+//
+
+#ifndef maman14_parser_h
+#define maman14_parser_h
+
+
+
+#endif
